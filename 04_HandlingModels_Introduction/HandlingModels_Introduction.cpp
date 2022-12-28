f1ab84891ab51ba938db5997e58e859fd15ef256
Javier Sotres <javier.sotres@sartorius.com>
2022-12-28 10:42:19 +0100

    c

---
+++ 04_HandlingModels_Introduction/HandlingModels_Introduction.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include "SIMCAQP.h"
+
+int main(int argc,char* argv[])
+{
+  // Check that only one input argument was passed when calling the script
+  // i.e., the name of the SIMCA project/file to be opened. Otherwise stop the program
+  if(argc==1)
+    {
+      std::cout<<"\nYou need to pass a SIMCA file as an argument\n";
+      return -1;
+    }
+  else if(argc>2)
+    {
+      std::cout<<"\nToo many arguments\n";
+      return -1;
+    }
+
+  SQ_ErrorCode eError; // handler for SIMCA-Q errors
+  char szError[256]; // C-string for handling SIMCA-Q error descriptions
+
+  // Load the project
+  SQ_Project hProject = NULL;
+  SQ_ErrorCode eError; // handler for SIMCA-Q erros
+  const char * szUSPFile = argv[1];
+  const char * szPassword = NULL;
+  eError = SQ_OpenProject(szUSPFile, szPassword, &hProject);
+  char szError[256]; // C-string for handling SIMCA-Q error descriptions
+  if (eError != SQ_E_OK)
+    {            
+      SQ_GetErrorDescription(eError, szError, sizeof(szError));
+      std::cout << szError << std::endl;
+      return -1;
+    }
+  
+  // Load the model with index = 1
+  SQ_Model hModel = NULL;
+  int iModelIndex = 1;
+  int iModelNumber = -1;
+  SQ_Bool bIsFitted;
+  SQ_GetModelNumberFromIndex(hProject, iModelIndex, &iModelNumber);
+  SQ_GetModel(hProject, iModelNumber, &hModel);
+  // Check if model is correct (=fitted) 
+  if (SQ_IsModelFitted(hModel, &bIsFitted) != SQ_E_OK || bIsFitted != SQ_True)
+    return -1;
+
+  // Print model properties
+
+  char szBuffer[256];
+  int iNumComponents;
+
+  // Model type 
+  eError = SQ_GetModelTypeString(hModel, szBuffer, sizeof(szBuffer));
+  std::cout<<"The model type is: "<< szBuffer <<"."<<std::endl;
+  
+  // Model name
+  eError = SQ_GetModelName(hModel, szBuffer, sizeof(szBuffer));
+  std::cout<<"The name of the model is: "<< szBuffer <<"."<<std::endl;
+
+  // Number of components
+  eError = SQ_GetNumberOfComponents(hModel, &iNumComponents);
+  std::cout<<"The number of components in the model is: "<< iNumComponents <<"."<<std::endl;
+
+  return 0;
+}
